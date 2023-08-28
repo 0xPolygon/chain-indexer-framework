90a4480868d66f882f60709c892dd93fd765418a
nivida <sam@aragon.one>
2023-08-28 14:01:09 +0200

    type declaration fixed

--- internal/kafka/producer/synchronous_producer.ts
+++ internal/kafka/producer/synchronous_producer.ts
@@ -59,8 +59,8 @@ export class SynchronousProducer extends AbstractProducer {
         timestamp?: number
     ): Promise<DeliveryReport | KafkaError> {
         return new Promise(async (resolve, reject) => {
-            let deliveryListener: (error: LibrdKafkaError, report: DeliveryReport) => void;
-            
+            let deliveryListener: undefined | ((error: LibrdKafkaError, report: DeliveryReport) => void);
+
             try {
                 const identifier = { time: Date.now() };
                 const timer = setTimeout(() => {
@@ -89,7 +89,9 @@ export class SynchronousProducer extends AbstractProducer {
                         return;
                     }
                 };
+
                 this.on("delivery-report", deliveryListener);
+                
                 await this.sendToInternalProducer(
                     key,
                     message,
@@ -103,7 +105,9 @@ export class SynchronousProducer extends AbstractProducer {
                     this.poll();
                 }, 100);
             } catch (error) {
-                this.removeListener("delivery-report", deliveryListener);
+                if (deliveryListener) {
+                    this.removeListener("delivery-report", deliveryListener);
+                }
 
                 throw KafkaError.createUnknown(error);
             }
