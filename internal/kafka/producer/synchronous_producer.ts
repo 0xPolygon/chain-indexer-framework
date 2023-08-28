5c6d229e509444b3911a821b7ca8aa726f705c7d
nivida <sam@aragon.one>
2023-08-28 13:51:12 +0200

    Empty arrow function removed

--- internal/kafka/producer/synchronous_producer.ts
+++ internal/kafka/producer/synchronous_producer.ts
@@ -59,7 +59,8 @@ export class SynchronousProducer extends AbstractProducer {
         timestamp?: number
     ): Promise<DeliveryReport | KafkaError> {
         return new Promise(async (resolve, reject) => {
-            let deliveryListener: (error: LibrdKafkaError, report: DeliveryReport) => void = () => { };
+            let deliveryListener: (error: LibrdKafkaError, report: DeliveryReport) => void;
+            
             try {
                 const identifier = { time: Date.now() };
                 const timer = setTimeout(() => {
