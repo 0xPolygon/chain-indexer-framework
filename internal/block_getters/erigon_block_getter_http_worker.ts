d4012c4cff5db3ccb268cc292c2f3272652a4fee
Nitin Mittal <nitinmittal44066@gmail.com>
2025-09-01 01:06:13 +0400

    feat: erigon block poller producer

---
+++ internal/block_getters/erigon_block_getter_http_worker.ts
@@ -0,0 +1,35 @@
+import { IBlockWorkerMessage } from "../interfaces/block_worker_message.js";
+import { parentPort, workerData } from "worker_threads";
+import { ErigonBlockGetter } from "./erigon_block_getter.js";
+import Eth from "web3-eth";
+
+if (!workerData || !parentPort) {
+    process.exit(1);
+}
+
+const blockGetter = new ErigonBlockGetter(
+    //@ts-ignore
+    new Eth(workerData.endpoint), workerData.maxRetries
+);
+
+parentPort.on("message", async (message: {
+    blockNumber: number, 
+    callBackId: number
+}) => {
+    try {
+        parentPort?.postMessage(
+            {
+                callBackId: message.callBackId,
+                error: null,
+                block: await blockGetter.getBlockWithTransactionReceipts(message.blockNumber)
+            } as IBlockWorkerMessage
+        );
+    } catch (error) {
+        parentPort?.postMessage(
+            {
+                callBackId: message.callBackId,
+                error: error
+            } as IBlockWorkerMessage
+        );
+    }
+});
---
+++ public/block_producers/erigon_poller_producer.ts
@@ -0,0 +1,75 @@
+import { IProducedBlock, ProducedBlocksModel, IProducedBlocksModel } from "@internal/block_producers/produced_blocks_model.js";
+import { IBlockProducerConfig } from "@internal/interfaces/block_producer_config.js";
+import { IProducerConfig } from "@internal/interfaces/producer_config.js";
+import { BlockGetter } from "@internal/block_getters/block_getter.js";
+import { Coder } from "@internal/coder/protobuf_coder.js";
+import { Database } from "@internal/mongo/database.js";
+import Eth from "web3-eth";
+import { BlockProducer } from "@internal/block_producers/block_producer.js";
+import { ErigonBlockPoller } from "@internal/block_subscription/erigon_block_polling.js";
+
+/**
+ * Erigon block producer class which retrieves block from http erigon node
+ * for producing to kafka.
+ *  
+ * @author - Nitin Mittal
+ */
+export class ErigonPollerProducer extends BlockProducer {
+    /**
+     * @constructor
+     * 
+     * @param {IBlockProducerConfig} config
+     * 
+     * @returns {ErigonPollerProducer}
+     */
+    constructor(config: IBlockProducerConfig) {
+        const endpoints = config.rpcWsEndpoints ?? [];
+        const startBlock = config.startBlock ?? 0;
+        const mongoUrl = config.mongoUrl ?? "mongodb://localhost:27017/chain-indexer";
+        const dbCollection = config.dbCollection ?? "producedblocks";
+        const blockPollingTimeout = config.blockPollingTimeout ?? 2000;
+        const maxRetries = config.maxRetries ?? 0;
+        const maxReOrgDepth = config.maxReOrgDepth ?? 0;
+
+        // Has to be done or Kafka complains later
+        delete config.rpcWsEndpoints;
+        delete config.startBlock;
+        delete config.mongoUrl;
+        delete config.dbCollection;
+        delete config.maxReOrgDepth;
+        delete config.maxRetries;
+        delete config.blockPollingTimeout;
+
+        const database = new Database(mongoUrl);
+
+        const blockGetter = new BlockGetter(
+            //@ts-ignore
+            new Eth(endpoints[0]),
+            maxRetries
+        );
+
+        super(
+            new Coder(
+                "block",
+                "blockpackage",
+                "Block"
+            ),
+            config as IProducerConfig,
+            new ErigonBlockPoller(
+                endpoints,
+                blockGetter,
+                blockPollingTimeout,
+                maxRetries
+            ),
+            blockGetter,
+            database,
+            database.model<IProducedBlock, IProducedBlocksModel<IProducedBlock>>(
+                "ProducedBlocks",
+                ProducedBlocksModel,
+                dbCollection
+            ),
+            startBlock,
+            maxReOrgDepth
+        );
+    }
+}
